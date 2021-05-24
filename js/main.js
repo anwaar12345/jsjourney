998db46f249deaec58daed8d878a882cb3a05c2a
syedanwarshah <syed.anwar@nexuscorp-ltd.com>
2021-05-24 06:12:51 -0700

    switch

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
     btn.addEventListener('click',function(){
     let element = document.getElementById('demo');
     if(!toggles){
-        element.innerHTML = "wow "+Date('YYYY');
+        element.innerHTML = "wow "+ new Date().getFullYear();
         element.style.color = "red";
         toggles = true;
         return;
@@ -19,3 +19,32 @@
 
     let {name:var_name,age:var_age} = {name:"test",age:12}; 
     console.log(var_name);
+    let testing = "chapter";
+    console.log(testing.charAt(0));
+
+
+    switch (new Date().getDay()) {
+        case 0:
+          day = "Sunday";
+          break;
+        case 1:
+          day = "Monday";
+          break;
+        case 2:
+           day = "Tuesday";
+          break;
+        case 3:
+          day = "Wednesday";
+          break;
+        case 4:
+          day = "Thursday";
+          break;
+        case 5:
+          day = "Friday";
+          break;
+        case 6:
+          day = "Saturday";
+      }
+      console.log(day);
+
+      
