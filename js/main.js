c67bd2c914a17c4b45dcd0da4755183d21022080
syedanwarshah <syed.anwar@nexuscorp-ltd.com>
2021-05-24 05:49:35 -0700

    object destructuring and assignment to vars

--- js/main.js
+++ js/main.js
@@ -6,12 +6,16 @@
     let element = document.getElementById('demo');
     if(!toggles){
         element.innerHTML = "wow "+Date('YYYY');
+        element.style.color = "red";
         toggles = true;
         return;
     }
     if(toggles){
         element.innerHTML = "demo";
         toggles = false;
+        element.style.color = "black";
     }
     });
-   
+
+    let {name:var_name,age:var_age} = {name:"test",age:12}; 
+    console.log(var_name);
