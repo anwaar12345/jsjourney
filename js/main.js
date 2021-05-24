9f00edb352227f314466def7b020a1a94cf195d2
syedanwarshah <syed.anwar@nexuscorp-ltd.com>
2021-05-24 04:35:33 -0700

    toggle js

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+   window.onload = function () {
+    let toggles = false;
+    var btn = document.getElementById('clicker');
+    btn.addEventListener('click',function(){
+    let element = document.getElementById('demo');
+    if(!toggles){
+        element.innerHTML = "wow "+Date('YYYY');
+        toggles = true;
+        return;
+    }
+    if(toggles){
+        element.innerHTML = "demo";
+        toggles = false;
+    }
+    });
+  }
+    
