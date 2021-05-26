20098ae2b88988ee99c6222cdd3e3b55db20d8a3
syedanwarshah <syed.anwar@nexuscorp-ltd.com>
2021-05-26 05:06:59 -0700

    done with slice (new arr) and splice (ma)

--- js/smarter.js
+++ js/smarter.js
@@ -24,7 +24,8 @@ console.log(arr);
 var btns = document.getElementById('clicker');
 btns.addEventListener('click', function(){
 var show;
- arr.forEach(element => {
+var regx = /\w/;
+arr.forEach(element => {
      var shows = document.getElementById('inner');
     if(typeof(shows) != 'undefined' && shows != null){
        return;
@@ -33,10 +34,28 @@ var show;
         show.setAttribute('id','inner')
         show.innerHTML = "<p>"+element.name+"</p>"+ "<br><p>Age: "+ element.age+"</p>";
         document.getElementById('app').appendChild(show);
+        regx.test(document.getElementById('demo').innerHTML) ? document.getElementById('inner').style.color = "red" : "";
+        
     }
     
 });
 } );
+var sts = [];
+sts.push('shah')
+sts.push(26)
+console.log(sts);
+sts.pop();
+sts.unshift('se')
+sts.shift()
+console.log(sts)  
 
+console.log("************************ splice manipulates orignal array and slice creates new array ******************************")
+
+var fruits = ['Banana','Grapes','Mango','Apricot'];
+console.log(fruits,'oringnal');
+fruits.splice(1,0,'Strawberry');
+console.log(fruits,'splice');
+var newa = fruits.slice(1,3) 
+console.log(newa,'slice');
 
 
