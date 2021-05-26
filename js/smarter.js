d7ea64bda781b1f5e421ae27d0a72708f2bb1675
syedanwarshah <syed.anwar@nexuscorp-ltd.com>
2021-05-26 02:35:56 -0700

    created element using array associative and for each

--- js/smarter.js
+++ js/smarter.js
@@ -15,3 +15,28 @@ let a  = prompt("Enter name","Name");
 let c = prompt("Enter Age","22");
 
 console.log(`My Name is ${a} and my age is ${c}`);
+console.log('********************** JS ARRAYS and Array manipulation ******************************');
+
+var arr = [];
+
+arr.push({name:'Anwar Shah',age:26});
+console.log(arr);
+var btns = document.getElementById('clicker');
+btns.addEventListener('click', function(){
+var show;
+ arr.forEach(element => {
+     var shows = document.getElementById('inner');
+    if(typeof(shows) != 'undefined' && shows != null){
+       return;
+    } else{
+        show = document.createElement('div');
+        show.setAttribute('id','inner')
+        show.innerHTML = "<p>"+element.name+"</p>"+ "<br><p>Age: "+ element.age+"</p>";
+        document.getElementById('app').appendChild(show);
+    }
+    
+});
+} );
+
+
+
