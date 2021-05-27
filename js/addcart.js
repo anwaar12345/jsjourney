3d7eede6467af58132cf14b799d6722a2eedfdff
syedanwarshah <syed.anwar@nexuscorp-ltd.com>
2021-05-27 08:03:58 -0700

    cart in process

--- js/addcart.js
+++ js/addcart.js
@@ -2,9 +2,30 @@
         addToCart();
     });
 
+    function increment(){
+        var val = document.getElementById('quantity');
+        val.value++
+        var price = document.getElementById('price').innerHTML;
+        document.getElementById('price').innerHTML = 'Rs: '+(3000 * val.value);
+        console.log(price)
+    }
+    function decrement(thi){
+        var val1 = document.getElementById('quantity');
+        thi.disabled = false;
+        if(val1.value > 0){
+            --val1.value
+            console.log(val1.value,'p')
+            
+        }else{
+            // thi.disabled = true;
+            val1.value = 0;
+        }
+        var price = document.getElementById('price').innerHTML;
+        document.getElementById('price').innerHTML = 'Rs: '+(3000 * val1.value);
+        
+    }
     function addToCart(){
         var shows = document.getElementById('cart');
-       
         if(typeof(shows) == 'object' && shows == null){
             console.log(shows);
             var divNew =  document.createElement('div');
@@ -12,7 +33,8 @@
             divNew.setAttribute('class','cart');
             var divMain = document.getElementsByClassName('main');
             divMain[0].appendChild(divNew)
-            var divUpper = divNew.previousSibling.previousSibling.previousSibling.previousSibling.childNodes;
+            var divUpper = divNew.previousSibling.previousSibling.childNodes;
+          
             for(var i = 0; i< divUpper.length; i++){
                if(divUpper[i].nodeType == 1 && divUpper[i].innerHTML != "Add To Cart"){
                    
