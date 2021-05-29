64e1b664cb0fabc4e7b8d651955574ddc6ba916f
syedanwarshah <syed.anwar@nexuscorp-ltd.com>
2021-05-29 03:04:48 -0700

    array and obj traversing

--- js/addcart.js
+++ js/addcart.js
@@ -54,3 +54,41 @@
    
       
     }
+
+
+
+function Student(name,age){
+     this.name = name,
+     this.age = age
+ }
+Student.prototype.getDetails = function(){
+    console.log("Name: "+this.name+ "\nAge: " + this.age);
+ };
+ var syed = new Student("Syed",26);
+ 
+ console.log(syed);
+
+ var Anwarshah = new Student("Anwar Shah",26);
+ console.log(Anwarshah.getDetails());
+
+ let car = {
+    model: 2010,
+    company: "BMW",
+    price: 4000+"$"
+};
+let car_detail = [];
+for(var prop in car){
+    console.log( car[prop]);
+    car_detail[prop] = car[prop];
+}
+console.log(car_detail,'datas');
+var d = Object.keys(car_detail).map( function(e) {
+    return car_detail[e];
+});
+console.log(d)
+d.forEach(e => {
+    console.log(e,'s')
+})
+console.log(d)
+
+
